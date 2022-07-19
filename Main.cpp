7bca921b0911fa0a0db4bf92a799bd57a6fac3af
Dmitriy Petrov <petrov-dmitriy13@yandex.ru>
2022-07-19 17:49:22 +0300

    Implement the game in "Main.cpp ".

--- Main.cpp
+++ Main.cpp
@@ -1,6 +1,52 @@
 #include <iostream>
+#include <conio.h>
+#include "Game.h"
+#include "Music.h"
+
+using namespace std;
+
+void getSymbols(char& humanSym, char& botSym);
 
 int main()
 {
-	return 0;
+	cout << "~use the English layout~" << endl << endl;
+
+	cout << "Pick you'r symbol!" << endl;
+	cout << "[Q] - X" << endl;
+	cout << "[W] - 0" << endl;
+
+	char humanSym, botSym;
+	getSymbols(humanSym, botSym);
+
+	while (true) {
+		system("cls");
+		Game game(humanSym, botSym);
+		game.play();
+
+		cout << "Play again?" << endl;
+		cout << "[Y] - Yes" << endl;
+		cout << "[Other key] - No" << endl;
+		
+		if (toupper(_getch()) != 'Y')
+			return 0;
+	}
+}
+
+void getSymbols(char& humanSym, char& botSym) {
+	while (true) {
+		char input = toupper(_getch());
+		switch (input) {
+		case 'Q':
+			humanSym = 'X';
+			botSym = '0';
+			return;
+		case 'W':
+			humanSym = '0';
+			botSym = 'X';
+			return;
+		default:
+			Music::error();
+			break;
+		}
+	}
 }
